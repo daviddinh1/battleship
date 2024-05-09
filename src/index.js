9d6eba4d6804263e7080b81359c116d61661acdd
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2024-05-09 18:16:36 -0500

    Added gameboard features of checking if all ships have been sunk and allowing users to place their ships

--- src/index.js
+++ src/index.js
@@ -1,41 +1,71 @@
 import "./style.css";
 
-class battleShip {
-  constructor(length, hp, ifHit) {
+class BattleShip {
+  constructor(length) {
     this.length = length;
     this.hp = 0;
     this.ifHit = false;
   }
+
   hit() {
     if (this.hp < this.length) {
-      this.hp = this.hp + 1;
+      this.hp += 1;
     }
+    this.isSunk();
     return this.hp;
   }
+
   isSunk() {
     if (this.hp === this.length) {
-      return (this.ifHit = true);
+      this.ifHit = true;
     }
+    return this.ifHit;
   }
 }
 
 class GameBoard {
   constructor() {
     this.grid = this.initializeGrid();
+    this.ships = [];
   }
+
   initializeGrid() {
     const rows = 10;
     const cols = 10;
-    let gameB = [];
-    for (let i = 0; i < rows; i++) {
-      let row = [];
-      for (let j = 0; j < cols; j++) {
-        row.push(0);
+    return Array.from({ length: rows }, () => Array(cols).fill(null));
+  }
+
+  place(x, y, length, horizontal = true) {
+    const ship = new BattleShip(length);
+    this.ships.push(ship);
+
+    for (let i = 0; i < length; i++) {
+      if (horizontal) {
+        this.grid[x][y + i] = ship;
+      } else {
+        this.grid[x + i][y] = ship;
       }
-      gameB.push(row);
     }
-    return gameB;
+  }
+
+  receiveAttack(x, y) {
+    const cell = this.grid[x][y];
+    if (cell instanceof BattleShip) {
+      cell.hit();
+      this.grid[x][y] = "h"; // Mark the grid cell as hit
+      return true;
+    } else if (this.grid[x][y] === null) {
+      this.grid[x][y] = "m"; // Mark the grid cell as miss
+      return false;
+    } else if (this.grid[x][y] === "h" || this.grid[x][y] === "m") {
+      // Cell was already attacked
+      return false;
+    }
+  }
+
+  allShipsSunk() {
+    return this.ships.every((ship) => ship.isSunk());
   }
 }
 
-module.exports = { battleShip, GameBoard };
+module.exports = { BattleShip, GameBoard };
--- src/index.test.js
+++ src/index.test.js
@@ -1,52 +1,79 @@
-const { battleShip, GameBoard } = require("./index.js");
+const { BattleShip, GameBoard } = require("./index.js");
 
-test("does the battleShip class exist", () => {
-  expect(battleShip).toBeDefined();
+describe("BattleShip", () => {
+  it("should correctly record hits", () => {
+    const ship = new BattleShip(3);
+    ship.hit();
+    expect(ship.hp).toBe(1);
+    ship.hit();
+    expect(ship.hp).toBe(2);
+    ship.hit();
+    expect(ship.hp).toBe(3);
+  });
+
+  it("should not allow hits beyond its length", () => {
+    const ship = new BattleShip(2);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.hp).toBe(2);
+  });
+
+  it("should correctly determine if the ship is sunk", () => {
+    const ship = new BattleShip(2);
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
 });
 
-test("does the battleShip class contain the units we are using", () => {
-  const bs = new battleShip(3);
-  expect(bs.length).toBe(3);
-  expect(bs.hp).toBe(0);
-});
+describe("GameBoard", () => {
+  let gameBoard;
 
-test("does the hit function exist in the battleShip class", () => {
-  const bs = new battleShip(3);
-  expect(bs.hit()).toBe(1);
-});
+  beforeEach(() => {
+    gameBoard = new GameBoard();
+  });
 
-test("does the hit function decrease the HP in the battleship class", () => {
-  const bs = new battleShip(3);
-  bs.hit();
-  bs.hit();
-  expect(bs.hp).toBe(2);
-});
+  it("should initialize a 10x10 grid", () => {
+    expect(gameBoard.grid.length).toBe(10);
+    expect(gameBoard.grid[0].length).toBe(10);
+  });
 
-test("does the sunk function check if the ship has sunk", () => {
-  const bs = new battleShip(4);
-  bs.hit();
-  bs.hit();
-  bs.hit();
-  bs.hit();
-  expect(bs.isSunk()).toBe(true);
-});
+  it("should place ships at the specified coordinates", () => {
+    gameBoard.place(0, 0, 3, true);
+    expect(gameBoard.grid[0][0]).toBeInstanceOf(BattleShip);
+    expect(gameBoard.grid[0][1]).toBeInstanceOf(BattleShip);
+    expect(gameBoard.grid[0][2]).toBeInstanceOf(BattleShip);
+  });
 
-test("does the gameBoard class exist", () => {
-  expect(GameBoard).toBeDefined();
-});
+  it("should record a hit on a ship", () => {
+    gameBoard.place(0, 0, 3, true);
+    const result = gameBoard.receiveAttack(0, 0);
+    expect(result).toBe(true);
+    expect(gameBoard.grid[0][0]).toBe("h");
+  });
 
-test("does the gameboard class have a gameboard 2d array", () => {
-  const gb = new GameBoard();
-  const rows = 10;
-  const cols = 10;
+  it("should record a missed attack", () => {
+    const result = gameBoard.receiveAttack(0, 0);
+    expect(result).toBe(false);
+    expect(gameBoard.grid[0][0]).toBe("m");
+  });
 
-  const gridToBe = [];
-  for (let i = 0; i < rows; i++) {
-    let row = [];
-    for (let j = 0; j < cols; j++) {
-      row.push(0);
-    }
-    gridToBe.push(row);
-  }
-  expect(gb.grid).toEqual(gridToBe);
+  it("should report if all ships are sunk", () => {
+    gameBoard.place(0, 0, 1, true);
+    gameBoard.receiveAttack(0, 0);
+    expect(gameBoard.allShipsSunk()).toBe(true);
+
+    gameBoard.place(1, 0, 2, true);
+    gameBoard.receiveAttack(1, 0);
+    gameBoard.receiveAttack(1, 1);
+    expect(gameBoard.allShipsSunk()).toBe(true);
+  });
+
+  it("should report if not all ships are sunk", () => {
+    gameBoard.place(0, 0, 2, true);
+    gameBoard.receiveAttack(0, 0);
+    expect(gameBoard.allShipsSunk()).toBe(false);
+  });
 });
